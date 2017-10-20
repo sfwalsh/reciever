594f63f213fba94cb726581dca3cdcfff3c5cf15
Stephen Walsh <sfwalshappdev@gmail.com>
2017-10-20 16:33:36 +0700

    init repo

---
+++ Receiver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Receiver
+//
+//  Created by Stephen Walsh on 20/10/2017.
+//  Copyright © 2017 Greenby Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
